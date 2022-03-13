cfcd319c99b48fcb0e5babc3d62805a7481babaf
Stan2hssn <86244342+Stan2hssn@users.noreply.github.com>
2022-03-13 03:13:05 +0100

    test

--- src/js/giro.js
+++ src/js/giro.js
@@ -31,7 +31,7 @@ function letter() {
         let reverse = value.split('').reverse()
         for (let i = 0; i < reverse.length; i++) {
             const el = reverse[i];
-            text.innerHTML += `<span id="span${i+1}" >${el}</span>`
+            text.innerHTML += `<span id="span${i+1}" style="color: red;">${el}</span>`
         }
         for (let i = 0; i < children.length; i++) {
             const child = children[i];
@@ -55,6 +55,9 @@ function init() {
     }
 
     document.querySelector("#input").oninput = function () {
+
+        text.style.fontVariationSettings = "'wght' " + 400;
+
         function mobile() {
             if (screen.width < 1200) {
                 getAccel();
@@ -66,6 +69,8 @@ function init() {
         console.log(screen.width);
         listInput.shift();
         var value = document.querySelector("#input").value;
+
+
 
         listInput.push(value.split(""));
         spanGeneration(listInput[0])
